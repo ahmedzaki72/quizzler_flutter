a6f33286f6ffcc700717b8016d3d20dbb0af1f77
ahmedzaki72 <ahmedsobhy317@gmail.com>
2020-09-18 08:20:49 +0200

    adding quizzler flutter app

---
+++ lib/main.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+import 'quizBrain.dart';
+
+
+QuizBrain quizBrain = QuizBrain();
+
+void main() {
+  runApp(Quizzler());
+}
+
+class Quizzler extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+
+  void CheckAnswer (bool userpickAnswer){
+    bool correctAnswer = quizBrain.getQuestionAnswer();
+    if (correctAnswer == userpickAnswer) {
+      setState(() {
+        scoreKeeper.add(
+          Icon(Icons.check, color: Colors.green),
+        );
+        quizBrain.nextQuestion();
+      });
+    } else {
+      setState(() {
+        scoreKeeper.add(
+          Icon(
+            Icons.close,
+            color: Colors.red,
+          ),
+        );
+        quizBrain.nextQuestion();
+      });
+    }
+  }
+
+//  List<String> questions = [
+//    'You can lead a cow down stairs but not up stairs.',
+//    'Approximately one quarter of human bones are in the feet.',
+//    'A slug\'s blood is green.'
+//  ];
+//
+//  List<bool>  answer = [
+//    false,
+//    true,
+//    true
+//  ];
+//
+//  Question q1 = Question(q : 'You can lead a cow down stairs but not up stairs.', a : false);
+//  Question q2 = Question(q:  'Approximately one quarter of human bones are in the feet.', a: true);
+//  Question q3 = Question(q:  'A slug\'s blood is green.', a: true);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                quizBrain.getQuestionText(),
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              textColor: Colors.white,
+              color: Colors.green,
+              child: Text(
+                'True',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+                //The user picked true.
+                CheckAnswer(true);
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              color: Colors.red,
+              child: Text(
+                'False',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {
+                //The user picked false.
+                CheckAnswer(false);
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Row(
+            children: scoreKeeper,
+          ),
+        )
+      ],
+    );
+  }
+}
---
+++ lib/questions.dart
@@ -0,0 +1,29 @@
+class Question {
+
+  String questionText;
+  bool questionAnswer;
+
+  /// this is constructor
+  Question(String q , bool a) {
+    questionText = q ;
+    questionAnswer = a;
+  }
+}
+
+
+
+/// this is using in constructor named function
+
+//
+//class Question {
+//
+//  String questionText;
+//  bool questionAnswer;
+//
+//  /// this is constructor
+    /// using named function and i will pass this name q and name a when i want to creating objects
+//  Question({String q , bool a}) {
+//    questionText = q ;
+//    questionAnswer = a;
+//  }
+//}
